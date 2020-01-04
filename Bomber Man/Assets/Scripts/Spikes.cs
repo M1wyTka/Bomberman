c185dbeacde5f650a145232e3bce458fedb09e44
M1wyTka <megalagerspb@gmail.com>
2020-01-05 01:25:35 +0300

    Some room modification + spikes

---
+++ Bomber Man/Assets/Scripts/Spikes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public int damage = 1;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            collision.GetComponent<PlayerStats>().TakeDamage(damage);
+        if(collision.CompareTag("Enemy") || collision.CompareTag("Smart Enemy"))
+            collision.GetComponent<Enemy>().TakeDamage(damage);
+    }
+}
