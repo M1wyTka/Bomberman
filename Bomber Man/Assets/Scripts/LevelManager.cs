380f48a0ab77e47b96e6031e52e2c6d19006a5b3
M1wyTka <megalagerspb@gmail.com>
2020-04-01 19:18:08 +0300

    Level transaction between main menu and game modified

---
+++ Bomber Man/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance = null;
+
+    public Object sceneToLoad;
+
+    public Slider progessSlider;
+
+    public GameObject menu;
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+            Destroy(gameObject);
+
+    }
+
+    public void LoadGameLvl()
+    {
+        progessSlider.gameObject.SetActive(true);
+        menu.SetActive(false);
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad.name);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+            progessSlider.value = progress;
+
+            yield return null;
+        }
+    }
+}
--- Bomber Man/Assets/Scripts/StartMenu.cs
+++ Bomber Man/Assets/Scripts/StartMenu.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
-    public Object sceneToLoad;
-
     public void StartGame()
     {
-        SceneManager.LoadScene(sceneToLoad.name);
+        LevelManager.Instance.LoadGameLvl();
     }
 
     public void Quit()
